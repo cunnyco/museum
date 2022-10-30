1426f5cb2c8ba6bb86d0217434a16717fddd4810
tinarskii <s16123@acsp.ac.th>
2022-10-30 19:41:12 +0700

    docs: update README.md

--- README.md
+++ README.md
@@ -1,41 +1,14 @@
-<div>
-  <p align="center">
-    <img src="/public/logo.png" width="150px"  alt="logo"/>
-    <h1 align="center">Hardtail</h1>
-    <p align="center">
-      Template for building SolidJS web application with Tailwind CSS faster!
-    </p>
-  </p>
-</div>
+# AniMuseum
+✨Museum of Awesome Anime to watch✨
 
-# Setup
+# Contribution Guide
+- Fork this repository
+- Upload your anime image to `public/cover` directory
+- Add your anime to `src/AniList.ts`
+- Create a pull request
 
-First you could clone or fork this project or repository to your local machine.
-By using `git` or `degit` commands
-
-Git: `git clone https://github.com/thevvx/hardtail.git`  
-Degit: `degit tihevvx/hardtail`
-
-Then you need to install all required dependencies with pnpm (preferred) or yarn.
-By running `pnpm install` or `yarn` you will get all required dependencies.
-
-After that you could run `pnpm dev` or `yarn dev` to start the development server.
-
-# What's included
-
-- [SolidJS](https://solidjs.com/) that is built with [Vite](https://vitejs.org/)
-- [Tailwind CSS v3](https://tailwindcss.com/)
-- [Ko-Fi Buttons](https://ko-fi.com/)
-- [Font Awesome](https://fontawesome.com/)
-- [Google Fonts](https://fonts.google.com/)
-
-# Features
-
-- [ ] A dark mode theme support
-- [x] A basic modal component
-- [ ] Customizable error page
-- [x] Responsive design out of the box
-- [x] LocalStorage support
-- [ ] Cool modal transitions
-- [ ] Testing library
-- [x] Prettier and ESLint support
+# Contribution Rules
+- Enter a valid website url
+- Make sure the anime is not already in the list
+- Sort the anime list by alphabetical order
+- Streaming websites should be legal
--- src/AniList.ts
+++ src/AniList.ts
@@ -3,20 +3,6 @@ import { IAnimeList } from "./types/list.interface";
 export const AniList: Array<IAnimeList> = [
   {
     title: {
-      japanese: "スロウスタート",
-      romaji: "Slow Start",
-    },
-    website: {
-      info: "https://myanimelist.net/anime/35540/Slow_Start",
-      stream: "https://www.crunchyroll.com/series/GRE5G4DP6/slow-start",
-    },
-    description: "Hana Ichinose has enrolled in high school one year late due to studying to get in. It might not seem like a big deal, but it is to her. She’s off to a slow start, but she wants to work hard and catch up to everyone else!",
-    coverImage: "slow-start.jpg",
-    isEnded: true,
-    episodes: 12,
-  },
-  {
-    title: {
       japanese: "超次元ゲイム ネプテューヌ THE ANIMATION",
       romaji: "Choujigen Game Neptune The Animation",
     },
@@ -26,6 +12,20 @@ export const AniList: Array<IAnimeList> = [
     },
     description: "Choujigen Game Neptune The Animation follows Neptune and her friends' attempts at raising Shares, while dealing with an external threat that could spell the end of both the Goddesses and Gamindustri itself...",
     coverImage: "neptune.jpg",
+    isEnded: true,
+    episodes: 12,
+  },
+  {
+    title: {
+      japanese: "スロウスタート",
+      romaji: "Slow Start",
+    },
+    website: {
+      info: "https://myanimelist.net/anime/35540/Slow_Start",
+      stream: "https://www.crunchyroll.com/series/GRE5G4DP6/slow-start",
+    },
+    description: "Hana Ichinose has enrolled in high school one year late due to studying to get in. It might not seem like a big deal, but it is to her. She’s off to a slow start, but she wants to work hard and catch up to everyone else!",
+    coverImage: "slow-start.jpg",
     isEnded: true,
     episodes: 12,
   },
